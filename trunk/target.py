16ff2ee526abab971f050d12a5d184cf3314ba1a
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2008-02-16 14:07:48 +0000

    new ver
    git-svn-id: https://aqualid.googlecode.com/svn@77 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

---
+++ trunk/target.py
@@ -0,0 +1,26 @@
+
+import sys
+import platform
+
+
+class   Target:
+    
+    if sys.platform == 'cygwin':
+        os = 'cygwin'
+    else:
+        os = platform.system()
+    
+    os_release = platform.release()
+    
+    os_version = platform.version()
+    
+    machine = platform.machine()
+    
+    cpu = platform.processor()
+    
+    cpu_flags = []
+    
+    platform = sys.platform
+
+
+
