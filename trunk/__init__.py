6b4a9cc4b620e3ac223e7a7d0cfb375e009e51fa
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2008-05-09 06:32:30 +0000

    Removed trunk
    git-svn-id: https://aqualid.googlecode.com/svn@107 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- trunk/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from options import EnvOptions, Options, BoolOption, EnumOption, IntOption, StrOption, VersionOption, PathOption
-
-from builtin_options import BuiltinOptions
-
-from main import Env, BuildVariant, Build
