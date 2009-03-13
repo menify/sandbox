26b0d082f97adf1d86b0752e71162e14ce5f7415
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2009-03-13 23:33:50 +0000

    test mva class
    git-svn-id: https://aqualid.googlecode.com/svn@147 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- examples/sbe/tests/tools/test_size.cpp
+++ examples/sbe/tests/tools/test_size.cpp
@@ -14,6 +14,119 @@ typedef sbe::SizeImpl<double>   DoubleSize;
 
 //---------------------------------------------------------------------------//
 
+template <typename T, size_t  maxSize>
+class MovingAverage
+{
+public:
+    explicit inline MovingAverage( size_t  size = maxSize )
+    : _start(0)
+    , _size(size)
+    , _actualSize(0)
+    , _avg()
+    {}
+    
+    inline ~MovingAverage()
+    {}
+    
+    
+    void    append( T  val )
+    {
+        SBE_ASSERT(_actualSize <= _size);
+        
+        const size_t    valuePos = (_start + _actualSize) % maxSize;
+        
+        if (_actualSize == _size) {
+            _avg += val - _values[_start];
+            _start = (_start + 1) % maxSize;
+        
+        } else {
+            if (_actualSize > 0) {
+                _avg += val;
+            } else {
+                _avg = val;
+            }
+            
+            ++_actualSize;
+        }
+        
+        _values[valuePos] = val;
+    }
+    
+    void    setSize(size_t  size)
+    {
+        SBE_ASSERT((size > 0) && (size <= maxSize));
+        
+        for (; _actualSize > size; --_actualSize) {
+            _avg -= _values[_start];
+            _start = (_start + 1) % maxSize;
+        }
+        
+        _size = size;
+    }
+    
+    T   value()
+    {
+        SBE_ASSERT(_actualSize > 0);
+        return _avg / static_cast<T>(_actualSize);
+    }
+    
+    void    reset()
+    {
+        _actualSize = 0;
+    }
+
+private:
+    size_t  _start;
+    size_t  _size;
+    size_t  _actualSize;
+    T       _avg;
+    T       _values[maxSize];
+};
+
+static void     test_mva()
+{
+    MovingAverage<size_t,5>    avg(4);
+    
+    static size_t const  values[]       = {1,2,3,4,5,5,4,1,3,5};
+    static size_t const  avg4_values[]  = {1,1,2,2,3,4,4,3,3,3};
+    
+    for (size_t  i = 0; i < sizeof(values) / sizeof(values[0]); ++i)
+    {
+        avg.append( values[i] );
+        SBE_ASSERT( avg.value() == avg4_values[i] );
+    }
+    
+    avg.setSize(2);
+    SBE_ASSERT( avg.value() == 4 );
+    avg.setSize(3);
+    SBE_ASSERT( avg.value() == 4 );
+    avg.setSize(5);
+    SBE_ASSERT( avg.value() == 4 );
+    avg.setSize(2);
+    SBE_ASSERT( avg.value() == 4 );
+    
+    static size_t const  avg2_values[]   = {3,1,2,3,4,5,4,2,2,4};
+    
+    for (size_t  i = 0; i < sizeof(values) / sizeof(values[0]); ++i)
+    {
+        avg.append( values[i] );
+        SBE_ASSERT( avg.value() == avg2_values[i] );
+    }
+    
+    avg.setSize(5);
+    static size_t const  avg5_values[]   = {3,2,2,3,3,3,4,3,3,3};
+    
+    for (size_t  i = 0; i < sizeof(values) / sizeof(values[0]); ++i)
+    {
+        avg.append( values[i] );
+        std::cout << i << ") avg: " << avg.value() << " == " << avg5_values[i] << std::endl;
+        SBE_ASSERT( avg.value() == avg5_values[i] );
+    }
+}
+
+
+//---------------------------------------------------------------------------//
+
 static void     test_size_t( void )
 {
     Size    s1( 100, 20);
@@ -96,6 +209,8 @@ int main( void )
 {
     test_size_t();
     test_double();
+    
+    test_mva();
     
     return 0;
 }
