4ed77dba1d04d2e75b7c3df7d00b2242f065ba61
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2010-01-18 15:38:57 +0000

    don't add the current word into the autocomplete list
    git-svn-id: https://aqualid.googlecode.com/svn@180 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- tools/scite/lua/autoc.lua
+++ tools/scite/lua/autoc.lua
@@ -2,7 +2,7 @@ local IGNORE_CASE = false
 -- Number of chars to type before the autocomplete list appears:
 local MIN_PREFIX_LEN = 2
 -- Length of shortest word to add to the autocomplete list
-local MIN_IDENTIFIER_LEN = 4
+local MIN_IDENTIFIER_LEN = MIN_PREFIX_LEN + 1
 -- A list of string patterns for finding suggestions for the autocomplete menu.
 local IDENTIFIER_PATTERNS = {"[%a_][%w_]+", "[%a_][%w_.]*[%w_]", "[%a_][%w_-]*[%w_]"}
 
@@ -22,7 +22,7 @@ function buildNames()
     for i, pattern in ipairs(IDENTIFIER_PATTERNS) do
         for word in string.gmatch(text, pattern) do
             if string.len(word) >= MIN_IDENTIFIER_LEN then
-                names[word] = true
+                names[word] = normalize(word)
             end
         end
     end
@@ -36,16 +36,18 @@ function handleChar()
         local startPos = editor:WordStartPosition(pos, true)
         local len = pos - startPos
         if len >= MIN_PREFIX_LEN then
-            local prefix = editor:textrange(startPos, pos)
+            local prefix = normalize( editor:textrange(startPos, pos) )
             local menuItems = {}
-            for name, v in pairs(names) do
-                if normalize(string.sub(name, 1, len)) == normalize(prefix) then 
+            for name, normname in pairs(names) do
+                if prefix ~= normname and string.sub(normname, 1, len) == prefix then
                     table.insert(menuItems, name)
                 end
             end
             if notempty(menuItems) then
                 table.sort(menuItems)
                 editor:AutoCShow(len, table.concat(menuItems, " "))
+            else
+              editor:AutoCCancel()
             end
         end
 --~     end
