bfbe74a9ffaaa0dbbfd53c0482803191db8af832
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2010-01-26 13:53:07 +0000

    improved words highlighting
    git-svn-id: https://aqualid.googlecode.com/svn@192 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- tools/scite/lua/mark_word.lua
+++ tools/scite/lua/mark_word.lua
@@ -1,69 +1,143 @@
 
 scite_Command {
    'Mark word|markOccurrences|Alt+H',
-   'Unmark words|clearOccurrences|Alt+C',
+   'Unmark words|clearAllOccurrences|Alt+C',
 }
 
 local marked_words = {}
-local MARKER_STYLE_NUMER = 0
+local MARKER_COLORS = {0x0000FF, 0x00C000, 0xFF0000, 0xFF00FF, 0xFFFF00, 0x00FFFF }
+local MARKER_COUNTS = {}
+
+-- //---------------------------------------------------------------------------//
+
+local function  updateMarkerCounter( mark, marker_number )
+    current_mark_counter = MARKER_COUNTS[ marker_number ] or 0
+    if mark then
+      current_mark_counter = current_mark_counter + 1
+    else
+      current_mark_counter = current_mark_counter - 1
+    end
+    
+    MARKER_COUNTS[ marker_number ] = current_mark_counter
+end
+
+-- //---------------------------------------------------------------------------//
+
+local function  clearMarkerCounters()
+    for marker_number, color in ipairs(MARKER_COLORS) do
+      MARKER_COUNTS[marker_number] = 0
+    end
+end
+
+-- //---------------------------------------------------------------------------//
+
+local function  getMarkerNumber()
+    local min_marker_number = 1
+    local min_counter = nil
+    
+    for marker_number, counter in ipairs(MARKER_COUNTS) do
+        if (min_counter == nil) or (min_counter > counter) then
+          min_counter = counter
+          min_marker_number = marker_number
+        end
+    end
+    
+    return min_marker_number
+end
 
 -- //---------------------------------------------------------------------------//
 
 local isMarkStylesInitialized = false
 local function initMarkStyles()
-    if not isMarkStylesInitialized then
-        editor.IndicStyle[MARKER_STYLE_NUMER] = INDIC_ROUNDBOX
-        editor.IndicFore[MARKER_STYLE_NUMER] = 255
-        editor.IndicAlpha[MARKER_STYLE_NUMER] = 90
-        isMarkStylesInitialized = true
+    if isMarkStylesInitialized then
+      return
     end
+    
+    isMarkStylesInitialized = true
+    
+    for style_number, color in ipairs(MARKER_COLORS) do
+        editor.IndicStyle[ style_number ] = INDIC_ROUNDBOX
+        editor.IndicFore[ style_number ] = color
+        editor.IndicAlpha[ style_number ] = 90
+    end
+    
+    clearMarkerCounters()
 end
-
-scite_OnOpen( initMarkStyles )
 
 -- //---------------------------------------------------------------------------//
 
-function markText(start, length, mark )
+local function markText(start, length, mark, marker_number )
+    
     local current_mark_number = scite.SendEditor(SCI_GETINDICATORCURRENT)
-    scite.SendEditor(SCI_SETINDICATORCURRENT, MARKER_STYLE_NUMER )
+    scite.SendEditor(SCI_SETINDICATORCURRENT, marker_number )
+
     if mark then
         scite.SendEditor(SCI_INDICATORFILLRANGE, start, length )
     else
         scite.SendEditor(SCI_INDICATORCLEARRANGE, start, length )
     end
+    
     scite.SendEditor(SCI_SETINDICATORCURRENT, current_mark_number)
 end
 
 -- //---------------------------------------------------------------------------//
 
-function markUnmarkWord( word, mark )
-    marked_words[word] = mark or nil
+local function markUnmarkWord( word, mark )
+    
+    initMarkStyles()
+    
+    local marker_number = marked_words[word]
+    
+    if mark then
+      if marker_number == nil then
+        marker_number = getMarkerNumber()
+        marked_words[word] = marker_number
+      end
+    else
+      if marker_number == nil then
+        return
+      end
+      
+      marked_words[word] = nil
+    end
     
     local flags = SCFIND_WHOLEWORD
     local s,e = editor:findtext(word,flags,0)
     while s do
-        markText( s, e - s, mark )
+        updateMarkerCounter( mark, marker_number )
+        markText( s, e - s, mark, marker_number )
         s,e = editor:findtext(word,flags,e+1)
     end
 end
 
 -- //---------------------------------------------------------------------------//
 
-function clearOccurrences()
-    markText( 0, editor.Length, false )
-    marked_words = {}
+local function markUnmakrAll( mark )
+    
+    for word,marker_number in pairs(marked_words) do
+        markUnmarkWord( word, mark )
+    end
 end
 
 -- //---------------------------------------------------------------------------//
 
-function markAllOccurrences()
-    markText( 0, editor.Length, false )
-    for word,mark in pairs(marked_words) do
-        if mark ~= nil then markUnmarkWord( word, true ) end
+local function clearIndicators()
+    for marker_number, color in ipairs(MARKER_COLORS) do
+      markText( 0, editor.Length, false, marker_number )
     end
 end
 
-scite_OnSwitchFile( markAllOccurrences )
+-- //---------------------------------------------------------------------------//
+
+local function GetCurrentWord()
+    if editor.SelectionStart ~= editor.SelectionEnd then
+        return editor:GetSelText()
+    end
+    
+    local current_pos = editor.CurrentPos
+    return editor:textrange( editor:WordStartPosition(current_pos, true),
+                             editor:WordEndPosition(current_pos, true) )
+end
 
 -- //---------------------------------------------------------------------------//
 
@@ -74,12 +148,16 @@ end
 
 -- //---------------------------------------------------------------------------//
 
-function GetCurrentWord()
-    if editor.SelectionStart ~= editor.SelectionEnd then
-        return editor:GetSelText()
-    end
-    
-    local current_pos = editor.CurrentPos
-    return editor:textrange( editor:WordStartPosition(current_pos, true),
-                             editor:WordEndPosition(current_pos, true) )
+function clearAllOccurrences()
+    markUnmakrAll( false )
+    clearIndicators()
 end
+
+function markAllOccurrences()
+    clearIndicators()
+    clearMarkerCounters()
+    markUnmakrAll( true )
+end
+
+scite_OnSwitchFile( markAllOccurrences )
+scite_OnOpen( markAllOccurrences )
