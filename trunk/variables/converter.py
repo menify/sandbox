62c753e856da2e28992b0bd02956bce3020b6ed8
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2010-01-27 19:29:28 +0000

    git-svn-id: https://aqualid.googlecode.com/svn@193 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- trunk/variables/converter.py
+++ trunk/variables/converter.py
@@ -124,7 +124,6 @@ class CapitalStrTraits (TypeTraitsBase):
 
 #//---------------------------------------------------------------------------//
 
-
 class   EnumTypeTraits (TypeTraitsBase):
     
     def     __init__( self, value_type ):
@@ -208,7 +207,82 @@ class   EnumTypeTraits (TypeTraitsBase):
     def   getAliases( self ):
         return dict(self.aliases)
 
-#//---------------------------------------------------------------------------//
+#//===========================================================================//
+
+class   BoolTypeTraits (TypeTraitsBase):
+    
+    #//-------------------------------------------------------//
+    
+    __str_to_bool = {
+               'y': True,
+               'yes': True,
+               't': True,
+               '1': True,
+               'true': True,
+               'on': True,
+               'enabled' : True
+               
+               }
+    
+    #//-------------------------------------------------------//
+    
+    __invert_false = {
+               'n': 'yes',
+               'no': 'yes',
+               'f': 'true',
+               '0': 'true',
+               'false': 'true',
+               'off': 'on',
+               'disabled' : 'enabled',
+               'none' : 'true'
+             }
+    
+    #//-------------------------------------------------------//
+    
+    __invert_bool = __invert_true.copy()
+    __invert_bool.update( __invert_false )
+    
+    #//-------------------------------------------------------//
+    
+    def     __init__( self, value_type ):
+        super(EnumTypeTraits, self).__init__( value_type )
+        
+        self.value_type = int # don't care about value_type it's always 'int'
+    
+    #//-------------------------------------------------------//
+    
+    def     convert( self, value ):
+        value = super(EnumTypeTraits, self).convert( value )
+        
+        if type(value) is bool:
+            value = int(value)
+        
+        if type(value) is int:
+            value = int(not not value)
+        
+        else:
+            value_str = str(value).lower()
+            if invert_true.has_key( value_str ):
+                value = int(True)
+            elif invert_false.has_key( value_str ):
+                value = int(False)
+            else:
+                raise TypeError("Invalid value: %s " % value )
+        
+        return value
+    
+    #//-------------------------------------------------------//
+    
+    def   compare( self, value1, value2 ):
+        return cmp( value1, value2 )
+    
+    #//-------------------------------------------------------//
+    
+    def   hash( self, value ):
+        return hash( value )
+
+
+#//===========================================================================//
 
 def     createValueTraits( base_type, *traits ):
     return type('ValueTraits', traits, {} )( base_type )
