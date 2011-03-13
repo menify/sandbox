35fd8bba5824a0904027d1187db66a02870eafb2
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2011-03-13 22:28:37 +0000

    git-svn-id: https://aqualid.googlecode.com/svn@228 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- maq/values/file_value.py
+++ maq/values/file_value.py
@@ -1,14 +1,18 @@
 
-import Value
+import os
 import hashlib
+
+import Value
 
 #//===========================================================================//
 
-class   FileContent (object):
+class   FileContentChecksum (object):
     
-    __slots__ = ( 'checksums' )
+    __slots__ = ( 'size', 'checksum' )
     
     def   __init__( self, path ):
+        
+        size = os.stat( path ).st_size
         
         checksum = hashlib.md5()
         
@@ -16,46 +20,60 @@ class   FileContent (object):
             checksum.update( chunk )
         
         self.checksum = checksum
+        self.size = size
     
     #//-------------------------------------------------------//
     
-    def   __eq__( self, other ):          return self.checksum == other.checksum
-    def   __ne__( self, other ):          return self.checksum != other.checksum
-    def   __getstate__( self ):           return { 'checksum': self.checksum }
-    def   __setstate__( self, state ):    self.checksum = state['checksum']
+    def   __eq__( self, other ):          return (self.size == other.size) and (self.checksum == other.checksum)
+    def   __ne__( self, other ):          return (self.size != other.size) or (self.checksum != other.checksum)
+    def   __getstate__( self ):           return { 'size': self.size, 'checksum': self.checksum }
+    def   __setstate__( self, state ):    self.size = state['size']; self.checksum = state['checksum']
+
+#//===========================================================================//
+
+class   FileContentTimeStamp (object):
+    
+    __slots__ = ( 'size', 'modify_time' )
+    
+    def   __init__( self, path ):
+        stat = os.stat( path )
+        
+        self.size = stat.st_size
+        self.modify_time = stat.st_mtime
+    
+    #//-------------------------------------------------------//
+    
+    def   __eq__( self, other ):          return (self.size == other.size) and (self.modify_time == other.modify_time)
+    def   __ne__( self, other ):          return (self.size != other.size) or (self.modify_time != other.modify_time)
+    def   __getstate__( self ):           return { 'size': self.size, 'modify_time': self.modify_time }
+    def   __setstate__( self, state ):    self.size = state['size']; self.modify_time = state['modify_time']
 
 #//===========================================================================//
 
 class   File (Value):
     
-    __slots__ = ( 'path', 'size', 'content' )
+    __slots__ = ( 'path', 'content' )
     
-    def   __init__( self, path, content = FileContent ) :
+    def   __init__( self, path, file_content = FileContentChecksum ) :
         self.path = os.path.abspath( path )
-        self.content = content( path )
+        self.content = file_content( path )
     
     #//-------------------------------------------------------//
     
-    def   modified( self, other ):
-        return False
+    def   __getstate__( self ):
+        return { 'path': self.path, 'content' : self.content }
     
     #//-------------------------------------------------------//
     
-    def   __( self ):
-        return str()
+    def   __setstate__( self, state ):
+        self.path = state['path']
+        self.content = state['content']
     
     #//-------------------------------------------------------//
     
-    @classmethod
-    def   from_bytes( cls, bytes ):
-        return cls();
-   
-    
-    #//-------------------------------------------------------//
-    
-    def   __lt__( self, other):       return False
-    def   __le__( self, other):       return False
-    def   __eq__( self, other):       return False
-    def   __ne__( self, other):       return False
-    def   __gt__( self, other):       return False
-    def   __ge__( self, other):       return False
+    def   __lt__( self, other):       return self.path < other.path
+    def   __le__( self, other):       return self.path <= other.path
+    def   __eq__( self, other):       return self.path == other.path
+    def   __ne__( self, other):       return self.path != other.path
+    def   __gt__( self, other):       return self.path > other.path
+    def   __ge__( self, other):       return self.path >= other.path
