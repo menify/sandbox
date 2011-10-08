fa0d21224370885aae886b60c16bcff60282ef11
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2011-10-08 21:17:10 +0000

    git-svn-id: https://aqualid.googlecode.com/svn@283 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- maq/utils/aql_logging.py
+++ maq/utils/aql_logging.py
@@ -20,6 +20,10 @@ def   _init():
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
     handler.setLevel( logging.DEBUG )
+    
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+    
     logger.addHandler( handler )
     
     global _logger
