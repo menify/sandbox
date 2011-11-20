64f79c173b8ed26bbe0fee303893f1b48ce480d6
menify <menify@a28edc5c-ec3e-0410-a3da-1b30b3a8704b>
2011-11-20 19:20:48 +0000

    git-svn-id: https://aqualid.googlecode.com/svn@320 a28edc5c-ec3e-0410-a3da-1b30b3a8704b

--- maq/values/aql_values_file.py
+++ maq/values/aql_values_file.py
@@ -69,6 +69,11 @@ class DependsKeys (object):
   
   #//-------------------------------------------------------//
   
+  def   __getitem__(self, dep_key ):
+    return self.deps[ dep_key ]
+  
+  #//-------------------------------------------------------//
+  
   def   clear(self):
     self.deps.clear()
     self.values.clear()
@@ -117,8 +122,8 @@ class ValuesFile (object):
   
   def   __getKeysOfValues( self, values ):
   
-  value_keys = []
-  value_keys_append = value_keys.append
+  value_keys = set()
+  value_keys_append = value_keys.add
   
   findValue = self.xash.find
   try:
@@ -181,8 +186,7 @@ class ValuesFile (object):
     xash = self.xash
     
     for key, dep_keys_value in sorted_deps:
-      values_keys = dep_keys_value.content
-      values = self.__getValuesByKeys( values_keys )
+      values = self.__getValuesByKeys( dep_keys_value.content )
       dep_value = DependsValue( dep_keys_value.name, values )
       
       xash[ key ] = dep_value
@@ -193,7 +197,6 @@ class ValuesFile (object):
   
   def   __removedDepends( self, removed_deps ):
     if removed_keys:
-      logWarning("DataFile is unsynchronized or internal error")
       xash = self.xash
       replace = self.data_file.replace
       dumps = self.dumps
@@ -208,45 +211,52 @@ class ValuesFile (object):
   
   def   __init__( self, filename ):
     self.xash = Xash()
-    self.deps = {}
+    self.deps = DependsKeys()
     self.data_file = None
     self.pickler = ValuePickler()
     self.open( filename )
+    self.loads = self.pickler.loads
+    self.dumps = self.pickler.dumps
+  
+  #//-------------------------------------------------------//
+  
+  def   __loadValue( self, key, data, dep_values ):
+    
+    value = self.loads( data )
+    
+    if isinstance( value, DependsValue ):
+      try:
+        dep_values[ key ] = [value, set(value.content)]
+      except TypeError:
+        self.xash[ key ] = value
+    else:
+      self.xash[ key ] = value
   
   #//-------------------------------------------------------//
   
   def   open( self, filename ):
     
     lock = FileLock( filename )
-    
     self.lock = lock
     
     with lock.readLock():
       self.data_file = DataFile( filename )
       
       dep_values = {}
-      loads = self.pickler.loads
-      
       for key, data in self.data_file:
-        value = loads( data )
-        
-        if isinstance( value, DependsValue ):
-          dep_values[ key ] = [value, set(value.content)]
-        
-        else:
-          self.xash[ key ] = value
+        self.__loadValue( key, data, dep_values )
       
       self.__restoreDepends( dep_values )
   
   #//-------------------------------------------------------//
   
-  def   update( self ):
+  def   __update( self ):
     
     xash = self.xash
     data_file = self.data_file
     loads = self.pickler.loads
     
-    added_keys, deleted_keys = self.data_file.update()
+    added_keys, deleted_keys = data_file.update()
     
     #//-------------------------------------------------------//
     
@@ -259,7 +269,7 @@ class ValuesFile (object):
     removed_keys -= deleted_keys
     
     if removed_keys:
-      logWarning("DataFile is unsynchronized or internal error")
+      logWarning("DataFile is unsynchronized")
       self.__removedDepends( removed_keys )
     
     #//-------------------------------------------------------//
@@ -267,13 +277,7 @@ class ValuesFile (object):
     dep_values = {}
     
     for key in added_keys:
-      value = loads( data_file[key] )
-      
-      if isinstance( value, DependsValue ):
-          dep_values[ key ] = [value, set(value.content)]
-      
-      else:
-        xash[ key ] = value
+      self.__loadValue( key, data_file[key], dep_values )
     
     self.__restoreDepends( dep_values )
   
@@ -291,8 +295,8 @@ class ValuesFile (object):
   
   def   findValues( self, values ):
     
-    with self.lock.readLock():
-      self.update()
+    with self.lock.writeLock():
+      self.__update()
     
     out_values = []
     
@@ -308,60 +312,59 @@ class ValuesFile (object):
   
   #//-------------------------------------------------------//
   
+  def   __addValue( self, value ):
+    key, val = xash.find( value )
+    if val is not None:
+      if value.content != val.content:
+        new_key = data_file.replace( key, self.dumps( value ) )
+        xash[ new_key ] = value
+        
+        removed_keys = deps.remove( key )
+        self.__removedDepends( removed_keys )
+    else:
+      key = data_file.append( self.dumps( value ) )
+      xash[key] = value
+  
+  #//-------------------------------------------------------//
+  
+  def   __addDepValue( self, value ):
+    
+    deps = self.deps
+    dep_value = DependsValue( value.name, self.__getKeysOfValues( value.content ) )
+    
+    key, val = xash.find( value )
+    if val is not None:
+      if deps[ key ] != dep_value.content:
+        data = self.dumps( dep_value )
+        
+        new_key = self.data_file.replace( key, data )
+        xash[ new_key ] = dep_value
+        
+        removed_keys = deps.remove( key )
+        self.__removedDepends( removed_keys )
+        
+        deps[ key ] = content_keys
+        
+    else:
+      data = self.dumps( dep_value )
+      key = self.data_file.append( data )
+      xash[key] = value
+      deps[ key ] = content_keys
+  
+  #//-------------------------------------------------------//
+  
   def   addValues( self, values ):
     
     values, dep_values = self.__sortValues( values )
     
     with self.lock.writeLock():
-      self.update()
-      
-      data_file = self.data_file
-      xash = self.xash
-      
-      valToKeys = self.__getKeysOfValues
-      dumps = self.dumps
-      
-      #//-------------------------------------------------------//
+      self.__update()
       
       for value in values:
-        key, val = xash.find( value )
-        if val is not None:
-          if value.content != val.content:
-            new_key = data_file.replace( key, dumps( value ) )
-            xash[ new_key ] = value
-            
-            removed_keys = self.deps.remove( key )
-            self.__removedDepends( removed_keys )
-            
-        else:
-          key = data_file.append( dumps( value ) )
-          xash[key] = value
-      
-      #//-------------------------------------------------------//
+        self.__addValue( value )
       
       for dep_value in dep_values:
-        key, val = xash.find( value )
-        if val is not None:
-          content_keys = valToKeys( dep_value.content )
-          if self.deps.deps[ key ] != set(content_keys):
-            data = dumps( DependsValue( value.name, content_keys ) )
-            
-            new_key = data_file.replace( key, data )
-            xash[ new_key ] = dep_value
-            
-            removed_keys = self.deps.remove( key )
-            self.__removedDepends( removed_keys )
-            
-            self.deps[ key ] = content_keys
-            
-        else:
-          content_keys = valToKeys( dep_value.content )
-          data = dumps( DependsValue( value.name, content_keys ) )
-          
-          key = data_file.append( data )
-          xash[key] = value
-          self.deps[ key ] = content_keys
-          
+        self.__addDepValue( dep_value )
   
   #//-------------------------------------------------------//
   
